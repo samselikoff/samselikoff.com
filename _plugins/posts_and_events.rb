e26e663ef8b5a42fd42ace57acc8c5ee9bd62145
Sam Selikoff <sam.selikoff@gmail.com>
2014-08-27 08:38:48 -0400

    Dynamic icons based on story type

--- _plugins/posts_and_events.rb
+++ _plugins/posts_and_events.rb
@@ -1,7 +1,8 @@
 module PostsAndEvents
   class Generator < Jekyll::Generator
     def generate(site)
-      posts_and_events = site.posts.map(&:to_liquid).concat site.data['events']
+      posts_hashes = site.posts.map(&:to_liquid).each{ |post| post['type'] = 'post'}
+      posts_and_events = posts_hashes.concat site.data['events']
 
       site.data['posts_and_events'] = posts_and_events.sort_by{ |item| item['date'] }.reverse
     end
