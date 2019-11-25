8a9defa3436d303bb4db516152dcd80d2e31813e
Sam Selikoff <sam.selikoff@gmail.com>
2019-11-24 22:39:37 -0500

    Talks + interviews

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -8,44 +8,54 @@ export default function TalksPage() {
 
       <div className="mt-12">
         <Talk
+          title="Stop Coding: You Have a Product Gap"
+          presentedAt="EmberCamp 2018"
           image="talks/embercamp2018.png"
-          talkUrl="https://www.youtube.com/watch?v=dWu2PxfFcUI"
-          slidesUrl="https://www.slideshare.net/samselikoff/stop-coding-you-have-a-product-gap"
+          url="https://www.youtube.com/watch?v=dWu2PxfFcUI"
         />
       </div>
       <div className="mt-12">
         <Talk
+          title="Better Documentation with Addon Docs"
+          presentedAt="EmberConf 2018"
           image="talks/emberconf2018.png"
-          talkUrl="https://www.youtube.com/watch?v=PVzutIELrf4"
+          url="https://www.youtube.com/watch?v=PVzutIELrf4"
         />
       </div>
       <div className="mt-12">
         <Talk
+          title="Common Principles of UI Development"
+          presentedAt="Manhattan JS, May 2017"
           image="talks/uipatterns2017.png"
-          slidesUrl="https://www.slideshare.net/samselikoff/common-ui-patterns"
+          url="https://www.slideshare.net/samselikoff/common-ui-patterns"
         />
       </div>
       <div className="mt-12">
         <Talk
+          title="Bring Sanity to your Frontend Infrastucture with Ember JS"
+          presentedAt="EmberConf 2015"
           image="talks/emberconf2015.png"
-          talkUrl="https://www.youtube.com/watch?v=iwPsNTkyCcA"
-          slidesUrl="https://www.slideshare.net/mobile/samselikoff/frontend-infrastructure"
+          url="https://www.youtube.com/watch?v=iwPsNTkyCcA"
         />
       </div>
       <div className="mt-12">
         <Talk
+          title="Using D3 with Backbone, Angular and Ember"
+          presentedAt="OpenVis Conf 2014"
           image="talks/openvisconf2014.png"
-          talkUrl="https://www.youtube.com/watch?v=ca3pQWc2-Xs"
+          url="https://www.youtube.com/watch?v=ca3pQWc2-Xs"
         />
       </div>
       <div className="mt-12">
         <Talk
+          title="Introduction to D3"
+          presentedAt="Analytics BigData Cloud & Discovery Meetup, February 2014"
           image="talks/introtod32014.png"
-          talkUrl="https://www.youtube.com/watch?v=kFCDA1uzGFo"
+          url="https://www.youtube.com/watch?v=kFCDA1uzGFo"
         />
       </div>
 
-      <div className="mt-16">
+      <div className="mt-16 mb-32">
         <h1 className="text-3xl font-semibold text-center">Interviews</h1>
 
         <div className="mt-16">
@@ -107,40 +117,19 @@ export default function TalksPage() {
 }
 
 const Talk = props => {
-  let bestUrl = props.talkUrl || props.slidesUrl
-
-  let links = []
-  if (props.talkUrl) {
-    links.push({ label: "Talk", url: props.talkUrl })
-  }
-  if (props.slidesUrl) {
-    links.push({ label: "Slides", url: props.slidesUrl })
-  }
-
   return (
     <>
-      <a href={bestUrl}>
+      <a href={props.url}>
         <Img src={props.image} />
       </a>
-      <p className="mt-4 text-center">
-        {links
-          .map(link => (
-            <a
-              className="text-blue-500 font-medium"
-              key={link.url}
-              href={link.url}
-            >
-              {link.label}
-            </a>
-          ))
-          .reduce((prev, curr, index) => [
-            prev,
-            <span className="px-2" key={index}>
-              ·
-            </span>,
-            curr,
-          ])}
-      </p>
+      <div className="leading-snug">
+        <p className="mt-3 text-sm font-semibold text-gray-500">
+          {props.presentedAt}
+        </p>
+        <p className="mt-1 font-semibold text-lg">
+          <a href={props.url}>{props.title}</a>
+        </p>
+      </div>
     </>
   )
 }
