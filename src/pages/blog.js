7974d3862ae93edbe51706ab2beaaca58c0c25fc
Sam Selikoff <sam.selikoff@gmail.com>
2019-11-25 23:33:57 -0500

    Blog index page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,51 +1,48 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 
-export default function BlogPage() {
-  let articles = [
-    {
-      title: "Lowest Common Ancestor",
-      date: "December 8, 2015",
-    },
-    {
-      title:
-        "UX Burlington 2015: Why Enterprise software sucks (and how to unsuck it)",
-      date: "November 2, 2015",
-    },
-    { title: "UX Burlington 2015: Scaling UX", date: "November 2, 2015" },
-    {
-      title: "UX Burlington 2015: Making Design by Committee Work",
-      date: "November 2, 2015",
-    },
-    {
-      title: "UX Burlington 2015: Improving the Authoring Experience",
-      date: "November 2, 2015",
-    },
-    {
-      title: `UX Burlington 2015: Don't fear the research!`,
-      date: "November 2, 2015",
-    },
-    {
-      title: "UX Burlington 2015: Designing with Empathy",
-      date: "November 2, 2015",
-    },
-    { title: "Comment on data flow in Ember apps", date: "September 19, 2015" },
-    { title: "Interview with Ember Weekend", date: "August 19, 2015" },
-    { title: "Preparing for Ember 2.0", date: "May 29, 2015" },
-    { title: "HTTPS in a nutshell", date: "May 2, 2015" },
-  ]
+export default function BlogPage({ data }) {
+  let articles = data.allMdx.edges.map(edge => ({
+    title: edge.node.frontmatter.title,
+    date: edge.node.frontmatter.date,
+    url: `/${edge.node.parent.relativePath.replace("/index.mdx", "")}`,
+  }))
 
   return (
     <>
       <h1 className="text-3xl font-semibold text-center">Blog</h1>
 
       <ul className="mt-12 leading-tight">
-        {articles.map((article, i) => (
-          <li className="mt-6" key={i}>
+        {articles.map(article => (
+          <li className="mt-6" key={article.url}>
             <p className="text-xs font-medium text-gray-500">{article.date}</p>
-            <h2 className="mt-1 text-lg font-semibold">{article.title}</h2>
+            <Link to={article.url}>
+              <h2 className="mt-1 text-lg font-semibold">{article.title}</h2>
+            </Link>
           </li>
         ))}
       </ul>
     </>
   )
 }
+
+export const query = graphql`
+  query BlogIndexQuery {
+    allMdx {
+      edges {
+        node {
+          id
+          frontmatter {
+            date
+            title
+          }
+          parent {
+            ... on File {
+              relativePath
+            }
+          }
+        }
+      }
+    }
+  }
+`
