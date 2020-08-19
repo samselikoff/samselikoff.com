761bb1f085530979e1e5d829673248810b73d700
Sam Selikoff <sam.selikoff@gmail.com>
2020-08-19 14:09:23 -0400

    home page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,237 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { Lead, Title, Spacer, Container, A, Img } from "../components/ui";
+import { Twitter, GitHub, YouTube } from "../components/logos";
+import NextLink from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <Container size="some">
+        <Spacer size="xl" />
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Title>Hello!</Title>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Spacer size="lg" />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <Lead>
+          I'm Sam Selikoff, and since 2016 I've made a living teaching
+          JavaScript UI development on the web.
+        </Lead>
+      </Container>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      <div className="mt-6 sm:hidden">
+        <HomepageImage />
+      </div>
 
+      <Container size="some">
+        <div className="flex items-center justify-between mt-5 text-xs font-medium text-gray-700 sm:justify-start smm:justify-center smm:text-sm lg:text-base lg:mt-8">
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            href="https://twitter.com/samselikoff"
+            className="flex items-center hover:text-gray-800 smm:px-4 sm:pl-0 sm:pr-6 lg:pr-8"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <Twitter className="h-4 mr-2" />
+            <span>Twitter</span>
           </a>
-
           <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            href="https://www.youtube.com/user/samselikoff"
+            className="flex items-center hover:text-gray-800 smm:px-4 sm:pl-0 sm:pr-6 lg:pr-8"
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <YouTube className="h-4 mr-2" />
+            <span>YouTube</span>
+          </a>
+          <a
+            href="https://github.com/samselikoff"
+            className="flex items-center hover:text-gray-800 smm:px-4 sm:pl-0 sm:pr-6 lg:pr-8"
+          >
+            <GitHub className="h-4 mr-2" />
+            <span>GitHub</span>
           </a>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        <div className="hidden mt-12 sm:block">
+          <HomepageImage className="rounded-lg" />
+        </div>
+
+        <div className="mt-16 md:text-lg lg:flex lg:flex-wrap lg:-mx-4">
+          <div className="lg:w-1/2 lg:px-4">
+            <Section>
+              <SectionTitle link="/projects">Projects</SectionTitle>
+              <SectionBody>
+                <p>
+                  I'm currently working on{" "}
+                  <A href="https://miragejs.com/">Mirage JS</A>, an API mocking
+                  library that lets frontend developers build complete features
+                  without touching their backend APIs.{" "}
+                </p>
+                <p className="mt-6">
+                  I also run <A href="https://embermap.com/">embermap.com</A>{" "}
+                  where I make videos about design, development and testing
+                  using Ember.js, along with my friend{" "}
+                  <A href="https://twitter.com/ryantotweets">Ryan Toronto</A>.
+                </p>
+              </SectionBody>
+            </Section>
+          </div>
+
+          <div className="lg:w-1/2 lg:px-4">
+            <Section>
+              <SectionTitle link="/podcast">Podcast</SectionTitle>
+              <SectionBody>
+                <p>
+                  Join me, Ryan + guests on a weekly podcast where we chat about
+                  all things JavaScript UI development!
+                </p>
+                <p className="mt-6">
+                  <A href="https://frontendfirst.fm/">
+                    → Check out Frontend First
+                  </A>
+                </p>
+              </SectionBody>
+            </Section>
+          </div>
+
+          <div className="lg:w-1/2 lg:px-4">
+            <Section>
+              <SectionTitle link="/talks">Talks & Interviews</SectionTitle>
+              <SectionBody>
+                <p>
+                  I've given several conference talks and talked about my open
+                  source work on a few podcasts.{" "}
+                  <A href="/talks">Check them out here.</A>
+                </p>
+              </SectionBody>
+            </Section>
+          </div>
+
+          <div className="lg:w-1/2 lg:px-4">
+            <Section>
+              <SectionTitle link="/blog">Blog</SectionTitle>
+              <SectionBody>
+                <p>
+                  You can find my writing on <A href="/blog">my blog</A>. I
+                  write mostly about frontend web development.
+                </p>
+              </SectionBody>
+            </Section>
+          </div>
+        </div>
+      </Container>
+
+      <div className="mt-20 md:mt-32 lg:mt-48">
+        <Container size="some">
+          <Title size="sm">Life</Title>
+          <Lead>
+            I was born in upstate New York, lived in Florida for 13 years, and
+            have since made my way back to the Northeast.
+          </Lead>
+          <Lead>
+            I currently live in New York City + absolutely love it here!
+          </Lead>
+        </Container>
+      </div>
+
+      <div className="mx-auto mt-16 xl:max-w-6xl">
+        <ImageCard
+          src="/images/new-york.jpeg"
+          title="New York City"
+          date="2015–Present"
+        />
+        <div className="flex xl:mt-10 xl:-mx-6">
+          <div className="w-1/2 xl:px-6">
+            <ImageCard
+              src="/images/burlington.jpeg"
+              title="Burlington"
+              date="2014–2015"
+              aspectRatio={1}
+            />
+          </div>
+          <div className="w-1/2 xl:px-6">
+            <ImageCard
+              src="/images/boston.jpg"
+              title="Boston"
+              date="2010–2014"
+              aspectRatio={1}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-16" />
+    </>
+  );
+}
+
+function HomepageImage({ className }) {
+  return (
+    <Img
+      src="/images/sam.jpeg"
+      aspectRatio={1616 / 1080}
+      className={`${className} h-full w-full object-cover object-top`}
+    />
+  );
+}
+function Section({ children }) {
+  return <section className="mt-10 md:mt-16 lg:mt-20">{children}</section>;
+}
+
+function SectionTitle({ link, children }) {
+  const T = ({ children }) => (
+    <h2 className="text-2xl font-semibold text-gray-900 md:text-2xl lg:text-2-5xl">
+      {children}
+    </h2>
+  );
+
+  if (link) {
+    return (
+      <NextLink href={link}>
+        <a className="inline-block">
+          <T>
+            {children}
+            <Chevron className="w-4 h-4 ml-1 md:w-5 md:h-5 lg:w-6 lg:h-6" />
+          </T>
         </a>
-      </footer>
+      </NextLink>
+    );
+  } else {
+    return <T>{children}</T>;
+  }
+}
+
+function SectionBody({ children }) {
+  return <div className="mt-2 md:mt-4 lg:mt-6">{children}</div>;
+}
+
+function Chevron(props) {
+  return (
+    <svg
+      className={`inline fill-current ${props.className}`}
+      viewBox="0 0 20 20"
+    >
+      <g id="Page-1" stroke="none" strokeWidth="1">
+        <g id="icon-shape">
+          <polygon
+            id="Combined-Shape"
+            points="12.9497475 10.7071068 13.6568542 10 8 4.34314575 6.58578644 5.75735931 10.8284271 10 6.58578644 14.2426407 8 15.6568542 12.9497475 10.7071068"
+          ></polygon>
+        </g>
+      </g>
+    </svg>
+  );
+}
+
+function ImageCard({ src, title, date, aspectRatio = 16 / 9 }) {
+  return (
+    <div className="relative">
+      <Img
+        className="object-cover w-full h-full xl:rounded-lg"
+        src={src}
+        aspectRatio={aspectRatio}
+      />
+      <div className="absolute bottom-0 w-full py-2 pl-3 text-white md:pl-4 md:py-4 bg-gradient-to-t from-black.85 xl:bg-none xl:static xl:text-gray-900 xl:px-0">
+        <p className="text-sm font-semibold sm:text-base xl:text-lg xl:font-medium">
+          {title}
+        </p>
+        <p className="text-xs sm:text-sm xl:text-gray-700">{date}</p>
+      </div>
     </div>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,124 @@
+function px(pixels) {
+  return `${pixels / 16}rem`;
+}
+
+module.exports = {
+  theme: {
+    screens: {
+      xs: "375px",
+      smm: "425px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+
+    fontSize: {
+      xs: px(12),
+      "sm-": px(13),
+      sm: px(14),
+      base: px(16),
+      "lg-": px(17),
+      lg: px(18),
+      lgg: px(19),
+      xl: px(20),
+      "1-5xl": px(22),
+      "2xl": px(24),
+      "2-5xl": px(27),
+      "3xl": px(30),
+      "4xl": px(36),
+      "4-5xl": px(42),
+      "5xl": px(48),
+      "6xl": px(64),
+      "7xl": px(80),
+      "8xl": px(96),
+    },
+
+    extend: {
+      backgroundImage: {
+        none: "none",
+      },
+      borderWidth: {
+        "3": "3px",
+      },
+      fontFamily: {
+        sans: `Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`,
+      },
+      spacing: {
+        "escape-xl": "calc((100vw -528px)/2)",
+      },
+      lineHeight: {
+        tighter: "1.15",
+      },
+      maxWidth: {
+        measure: "34em",
+        "7xl": "80rem",
+      },
+      width: {
+        28: "7rem",
+        36: "9rem",
+      },
+      letterSpacing: {
+        "tight-": "-0.0125em",
+      },
+      height: {
+        11: "2.75rem",
+        28: "7rem",
+        36: "9rem",
+      },
+      colors: {
+        "black.85": "rgba(0,0,0,0.85)",
+        gray: {
+          // Cool, but less cool than default: https://hihayk.github.io/scale/#4/4/82/91/0/0/-19/-5/a6afb9/166/175/185
+          // "100": "#F7F8F9",
+          // "200": "#E3E6E8",
+          // "300": "#CFD3D8",
+          // "400": "#BBC1C9",
+          // "500": "#A6AFB9",
+          // "600": "#848B93",
+          // "700": "#62676D",
+          // "800": "#404347",
+          // "900": "#1E2021",
+
+          // Another (darker darks): https://hihayk.github.io/scale/#4/4/91/91/0/0/-19/-5/a6afb9/166/175/185
+          // But swapped 900 with above
+          "100": "#F7F8F9",
+          "200": "#E3E6E8",
+          "300": "#CFD3D8",
+          "400": "#BBC1C9",
+          "500": "#A6AFB9",
+          "600": "#80878F",
+          "700": "#5B5F65",
+          "800": "#35383B",
+          "900": "#1E2021",
+        },
+      },
+    },
+  },
+
+  corePlugins: {
+    fontSize: false, // fonts added below
+  },
+
+  plugins: [
+    function ({ addUtilities, theme }) {
+      const fontSizes = theme("fontSize", {});
+
+      Object.keys(fontSizes).forEach((key) => {
+        let fontSize = fontSizes[key];
+        let pixels = +fontSize.replace("rem", "") * 16;
+        let tracking = -0.0223 + 0.185 * Math.exp(-0.1745 * pixels);
+
+        addUtilities(
+          {
+            [`.text-${key}`]: {
+              "font-size": fontSize,
+              "letter-spacing": `${tracking}rem`,
+            },
+          },
+          ["responsive"]
+        );
+      });
+    },
+  ],
+};
