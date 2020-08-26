1aeb16b7aa996606f47c97e1a3c6fd22df7c4189
Sam Selikoff <sam.selikoff@gmail.com>
2020-08-26 18:09:18 -0400

    moar

--- pages/work-journal-admin.js
+++ pages/work-journal-admin.js
@@ -1,5 +1,5 @@
 import { Head, Container, Spacer, Title, Lead } from "../components/ui";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import * as firebase from "firebase/app";
 // Add the Firebase products that you want to use
 import "firebase/database";
@@ -43,6 +43,38 @@ export default function WorkJournalAdminPage() {
     f();
   });
 
+  let [isSavingNewEntry, setIsSavingNewEntry] = useState();
+  let [newEntry, setNewEntry] = useState({
+    date: format(new Date(), "yyyy-MM-dd"),
+    text: "",
+    category: "work",
+    href: "",
+  });
+
+  function handleChange(key, value) {
+    setNewEntry((newEntry) => ({ ...newEntry, [key]: value }));
+  }
+
+  function createEntry(e) {
+    e.preventDefault();
+    setIsSavingNewEntry(true);
+
+    let db = firebaseApp.database();
+    let newId = db.ref().child("work-journal-entries").push().key;
+
+    db.ref(`work-journal-entries/${newId}`)
+      .set(newEntry)
+      .then(() => {
+        setIsSavingNewEntry(false);
+        setNewEntry({
+          date: format(new Date(), "yyyy-MM-dd"),
+          text: "",
+          href: "",
+          category: "work",
+        });
+      });
+  }
+
   return (
     <>
       <Head>
@@ -65,7 +97,12 @@ export default function WorkJournalAdminPage() {
             <p className="text-sm italic font-medium text-gray-500 uppercase">
               New entry
             </p>
-            <EntryForm />
+            <NewEntry
+              entry={newEntry}
+              isSaving={isSavingNewEntry}
+              handleChange={handleChange}
+              onSubmit={createEntry}
+            />
           </div>
 
           <Spacer size="lg" />
@@ -78,7 +115,7 @@ export default function WorkJournalAdminPage() {
             <ul className="divide-y divide-cool-gray-200">
               {entries.map((entry) => (
                 <div className="py-4" key={entry.id}>
-                  <EntryForm entry={entry} />
+                  <Entry entry={entry} />
                 </div>
               ))}
             </ul>
@@ -89,11 +126,62 @@ export default function WorkJournalAdminPage() {
   );
 }
 
-function EntryForm({ entry = { date: format(new Date(), "yyyy-MM-dd") } }) {
+function NewEntry({ entry, isSaving, handleChange, onSubmit }) {
+  return (
+    <EntryForm
+      entry={entry}
+      isSaving={isSaving}
+      onChange={handleChange}
+      onSubmit={onSubmit}
+    >
+      <div className="pt-5">
+        <div className="flex justify-end">
+          <span className="inline-flex rounded-md shadow-sm">
+            <button
+              type="submit"
+              className="relative flex items-center justify-center px-4 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-blue-500 border border-transparent rounded-md hover:bg-blue-400 focus:outline-none focus:border-blue-600 focus:shadow-outline-blue active:bg-blue-600"
+            >
+              <span
+                className={`${
+                  isSaving ? "" : "invisible"
+                } absolute inset-0 flex items-center justify-center`}
+              >
+                <svg
+                  className="w-4 h-4 text-white animate-spin"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              </span>
+
+              <span className={isSaving ? "invisible" : ""}>Create Entry</span>
+            </button>
+          </span>
+        </div>
+      </div>
+    </EntryForm>
+  );
+}
+
+function Entry({ entry }) {
   let [localEntry, setLocalEntry] = useState({ ...entry });
   let [isSaving, setIsSaving] = useState(false);
 
   function handleChange(key, value) {
+    console.log("here");
     setLocalEntry((localEntry) => ({ ...localEntry, [key]: value }));
   }
 
@@ -103,9 +191,7 @@ function EntryForm({ entry = { date: format(new Date(), "yyyy-MM-dd") } }) {
 
     let db = firebaseApp.database();
 
-    let id = entry.id || db.ref().child("work-journal-entries").push().key;
-
-    db.ref(`work-journal-entries/${id}`)
+    db.ref(`work-journal-entries/${entry.id}`)
       .set(localEntry)
       .then(() => {
         setIsSaving(false);
@@ -123,183 +209,219 @@ function EntryForm({ entry = { date: format(new Date(), "yyyy-MM-dd") } }) {
   let hasLocalChanges = !shallowEqual(entry, localEntry);
 
   return (
-    <div>
-      <form onSubmit={updateEntry}>
-        <div className="grid grid-cols-1 row-gap-6 col-gap-4 mt-6 sm:grid-cols-6">
-          <div className="sm:col-span-3">
-            <label
-              htmlFor={`entry${localEntry.id}-date`}
-              className="block text-sm font-medium leading-5 text-gray-700"
+    <EntryForm
+      entry={localEntry}
+      hasLocalChanges={hasLocalChanges}
+      isSaving={isSaving}
+      onChange={handleChange}
+      onSubmit={updateEntry}
+    >
+      <div className="pt-5">
+        <div className="flex">
+          <span className="inline-flex mr-auto rounded-md shadow-sm">
+            <button
+              onClick={deleteEntry}
+              type="button"
+              className="px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
             >
-              Date
-            </label>
-            <div className="mt-1 rounded-md shadow-sm">
-              <input
-                id={`entry${localEntry.id}-date`}
-                value={localEntry.date}
-                onChange={(e) => handleChange("date", e.target.value)}
-                className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-              />
-            </div>
-          </div>
+              Delete...
+            </button>
+          </span>
+          <span className="inline-flex rounded-md shadow-sm">
+            <button
+              type="submit"
+              disabled={!hasLocalChanges}
+              className={`${
+                hasLocalChanges ? "" : "pointer-events-none opacity-50"
+              } hinline-flex justify-center px-4 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-green-500 border border-transparent rounded-md hover:bg-green-400 focus:outline-none focus:border-green-600 focus:shadow-outline-green active:bg-green-600 flex items-center relative`}
+            >
+              <span
+                className={`${
+                  isSaving ? "" : "invisible"
+                } absolute inset-0 flex items-center justify-center`}
+              >
+                <svg
+                  className="w-4 h-4 text-white animate-spin"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+              </span>
+
+              <span className={isSaving ? "invisible" : ""}>Update</span>
+            </button>
+          </span>
         </div>
+      </div>
+    </EntryForm>
+  );
+}
 
-        <fieldset className="mt-8">
-          <legend className="text-sm font-medium leading-5 text-gray-700">
-            Category
-          </legend>
-          <div className="flex items-center mt-2">
-            <div className="flex items-center">
-              <input
-                id={`entry${localEntry.id}-work`}
-                name="category"
-                type="radio"
-                value="work"
-                checked={localEntry.category === "work"}
-                onChange={() => handleChange("category", "work")}
-                className="w-4 h-4 text-blue-600 transition duration-150 ease-in-out form-radio"
-              />
-              <label htmlFor={`entry${localEntry.id}-work`} className="ml-3">
-                <span className="block text-sm font-medium leading-5 ">
-                  Work
-                </span>
-              </label>
-            </div>
-            <div className="flex items-center ml-6">
-              <input
-                id={`entry${localEntry.id}-learning`}
-                name="category"
-                type="radio"
-                value="learning"
-                checked={localEntry.category === "learning"}
-                onChange={() => handleChange("category", "learning")}
-                className="w-4 h-4 text-blue-600 transition duration-150 ease-in-out form-radio"
-              />
-              <label
-                htmlFor={`entry${localEntry.id}-learning`}
-                className="ml-3"
-              >
-                <span className="block text-sm font-medium leading-5 ">
-                  Learning
-                </span>
-              </label>
-            </div>
-            <div className="flex items-center ml-6">
-              <input
-                id={`entry${localEntry.id}-interesting-thing`}
-                name="category"
-                type="radio"
-                value="interesting-thing"
-                checked={localEntry.category === "interesting-thing"}
-                onChange={() => handleChange("category", "interesting-thing")}
-                className="w-4 h-4 text-blue-600 transition duration-150 ease-in-out form-radio"
-              />
-              <label
-                htmlFor={`entry${localEntry.id}-interesting-thing`}
-                className="ml-3"
-              >
-                <span className="block text-sm font-medium leading-5 ">
-                  Interesting thing
-                </span>
-              </label>
-            </div>
-          </div>
-        </fieldset>
+function EntryForm({
+  entry,
+  hasLocalChanges,
+  isSaving,
+  onChange,
+  onSubmit,
+  children,
+}) {
+  // let [localEntry, setLocalEntry] = useState({ ...entry });
+  // let [isSaving, setIsSaving] = useState(false);
 
-        <div className="mt-8 sm:col-span-6">
+  // function handleChange (key, value) {
+  //   setLocalEntry((localEntry) => ({ ...localEntry, [key]: value }));
+  // };
+
+  // function updateEntry(e) {
+  //   e.preventDefault();
+  //   setIsSaving(true);
+
+  //   let db = firebaseApp.database();
+
+  //   let id = entry.id || db.ref().child("work-journal-entries").push().key;
+
+  //   db.ref(`work-journal-entries/${id}`)
+  //     .set(localEntry)
+  //     .then(() => {
+  //       setIsSaving(false);
+  //     });
+  // }
+
+  // function deleteEntry() {
+  //   if (confirm("Delete this entry?")) {
+  //     firebaseApp.database().ref(`work-journal-entries/${entry.id}`).remove();
+  //   } else {
+  //     // ignore
+  //   }
+  // }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="grid grid-cols-1 row-gap-6 col-gap-4 mt-6 sm:grid-cols-6">
+        <div className="sm:col-span-3">
           <label
-            htmlFor={`entry${localEntry.id}-text`}
+            htmlFor={`entry${entry.id}-date`}
             className="block text-sm font-medium leading-5 text-gray-700"
           >
-            Text
+            Date
           </label>
           <div className="mt-1 rounded-md shadow-sm">
-            <textarea
-              id={`entry${localEntry.id}-text`}
-              value={localEntry.text}
-              onChange={(e) => handleChange("text", e.target.value)}
-              rows="3"
-              className="block w-full transition duration-150 ease-in-out form-textarea sm:text-sm sm:leading-5"
-            ></textarea>
+            <input
+              id={`entry${entry.id}-date`}
+              value={entry.date}
+              onChange={(e) => onChange("date", e.target.value)}
+              className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
+            />
           </div>
         </div>
-
-        <div className="grid grid-cols-1 row-gap-6 col-gap-4 mt-6 sm:grid-cols-6">
-          <div className="sm:col-span-6">
-            <label
-              htmlFor={`entry${localEntry.id}-href`}
-              className="block text-sm font-medium leading-5 text-gray-700"
-            >
-              Link
+      </div>
+      <fieldset className="mt-8">
+        <legend className="text-sm font-medium leading-5 text-gray-700">
+          Category
+        </legend>
+        <div className="flex items-center mt-2">
+          <div className="flex items-center">
+            <input
+              id={`entry${entry.id}-work`}
+              name="category"
+              type="radio"
+              value="work"
+              checked={entry.category === "work"}
+              onChange={() => onChange("category", "work")}
+              className="w-4 h-4 text-blue-600 transition duration-150 ease-in-out form-radio"
+            />
+            <label htmlFor={`entry${entry.id}-work`} className="ml-3">
+              <span className="block text-sm font-medium leading-5 ">Work</span>
             </label>
-            <div className="mt-1 rounded-md shadow-sm">
-              <input
-                id={`entry${localEntry.id}-href`}
-                value={localEntry.href}
-                placeholder="http://some-url.com"
-                onChange={(e) => handleChange("href", e.target.value)}
-                className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
-              />
-            </div>
           </div>
-        </div>
-
-        <div className="pt-5">
-          <div className="flex flex-end">
-            {entry.id && (
-              <span className="inline-flex mr-auto rounded-md shadow-sm">
-                <button
-                  onClick={deleteEntry}
-                  type="button"
-                  className="px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out border border-gray-300 rounded-md hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-50 active:text-gray-800"
-                >
-                  Delete...
-                </button>
+          <div className="flex items-center ml-6">
+            <input
+              id={`entry${entry.id}-learning`}
+              name="category"
+              type="radio"
+              value="learning"
+              checked={entry.category === "learning"}
+              onChange={() => onChange("category", "learning")}
+              className="w-4 h-4 text-blue-600 transition duration-150 ease-in-out form-radio"
+            />
+            <label htmlFor={`entry${entry.id}-learning`} className="ml-3">
+              <span className="block text-sm font-medium leading-5 ">
+                Learning
               </span>
-            )}
-            <span className="inline-flex rounded-md shadow-sm">
-              <button
-                type="submit"
-                disabled={!hasLocalChanges}
-                className={`${
-                  hasLocalChanges ? "" : "pointer-events-none opacity-50"
-                } hinline-flex justify-center px-4 py-2 text-sm font-medium leading-5 text-white transition duration-150 ease-in-out bg-green-500 border border-transparent rounded-md hover:bg-green-400 focus:outline-none focus:border-green-600 focus:shadow-outline-green active:bg-green-600 flex items-center relative`}
-              >
-                <span
-                  className={`${
-                    isSaving ? "" : "invisible"
-                  } absolute inset-0 flex items-center justify-center`}
-                >
-                  <svg
-                    className="w-4 h-4 text-white animate-spin"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      strokeWidth="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
-                </span>
-
-                <span className={isSaving ? "invisible" : ""}>
-                  {entry.id ? "Update" : "Create"}
-                </span>
-              </button>
-            </span>
+            </label>
+          </div>
+          <div className="flex items-center ml-6">
+            <input
+              id={`entry${entry.id}-interesting-thing`}
+              name="category"
+              type="radio"
+              value="interesting-thing"
+              checked={entry.category === "interesting-thing"}
+              onChange={() => onChange("category", "interesting-thing")}
+              className="w-4 h-4 text-blue-600 transition duration-150 ease-in-out form-radio"
+            />
+            <label
+              htmlFor={`entry${entry.id}-interesting-thing`}
+              className="ml-3"
+            >
+              <span className="block text-sm font-medium leading-5 ">
+                Interesting thing
+              </span>
+            </label>
           </div>
         </div>
-      </form>
-    </div>
+      </fieldset>
+      <div className="mt-8 sm:col-span-6">
+        <label
+          htmlFor={`entry${entry.id}-text`}
+          className="block text-sm font-medium leading-5 text-gray-700"
+        >
+          Text
+        </label>
+        <div className="mt-1 rounded-md shadow-sm">
+          <textarea
+            id={`entry${entry.id}-text`}
+            value={entry.text}
+            onChange={(e) => onChange("text", e.target.value)}
+            rows="3"
+            className="block w-full transition duration-150 ease-in-out form-textarea sm:text-sm sm:leading-5"
+          ></textarea>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 row-gap-6 col-gap-4 mt-6 sm:grid-cols-6">
+        <div className="sm:col-span-6">
+          <label
+            htmlFor={`entry${entry.id}-href`}
+            className="block text-sm font-medium leading-5 text-gray-700"
+          >
+            Link
+          </label>
+          <div className="mt-1 rounded-md shadow-sm">
+            <input
+              id={`entry${entry.id}-href`}
+              value={entry.href}
+              placeholder="http://some-url.com"
+              onChange={(e) => onChange("href", e.target.value)}
+              className="block w-full transition duration-150 ease-in-out form-input sm:text-sm sm:leading-5"
+            />
+          </div>
+        </div>
+      </div>
+      {children}
+    </form>
   );
 }
 
