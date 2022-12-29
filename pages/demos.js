7d747ab94b43eb2a913119bf774011da10dbf985
Sam Selikoff <sam.selikoff@gmail.com>
2022-12-28 21:54:26 -0500

    wip

--- pages/demos.js
+++ pages/demos.js
@@ -33,6 +33,7 @@ export default function Demos() {
                       autoPlay
                       muted
                       loop
+                      playsInline
                     />
                   </a>
                 </div>
